9176110df42c28e340281dc54f1f2a8dbb6c5da2
sungin.Hong <nicehsi@naver.com>
2025-07-14 09:57:01 +0900

    Add Chromatic GitHub Actions workflow

--- .storybook/main.js
+++ .storybook/main.js
@@ -17,6 +17,13 @@ const config = {
   },
   "staticDirs": [
     "../public"
-  ]
+  ],
+  webpackFinal: async (config) => {
+    // ESLint 플러그인 제거하여 충돌 해결
+    config.plugins = config.plugins.filter(
+      plugin => plugin.constructor.name !== 'ESLintWebpackPlugin'
+    );
+    return config;
+  },
 };
 export default config;
